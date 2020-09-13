1a501af03d9f6d669701f690f7e45b1aa1dbfbe4
ekaxfrnd <ekaxfrnd@gmail.com>
2020-09-13 13:05:01 -0400

    main file for the app

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+
+require('dotenv').config()
+const logger = require('morgan')
+
+const app = express()
+
+const {
+    SESSION_SECRET,
+    SERVER_PORT,
+    SERVER_HOST
+} = process.env
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+const hbs = exphbs.create({
+    extname: '.hbs'
+})
+app.engine('.hbs', hbs.engine)
+app.set('view engine', '.hbs')
+app.use(flash())
+app.set(express.static('public'))
+
+app.use(logger('dev'))
+
+app.listen(SERVER_PORT, () => console.log(`started on: http://${SERVER_HOST}:${SERVER_PORT}`))
