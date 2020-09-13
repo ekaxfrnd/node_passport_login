7fb031405d94ccf1ada6239daf67ecc526b40358
ekaxfrnd <ekaxfrnd@gmail.com>
2020-09-13 13:09:42 -0400

    connection file to db

---
+++ config/conn.js
@@ -0,0 +1,18 @@
+const {
+    Sequelize
+} = require('sequelize')
+
+require('dotenv').config()
+
+const {
+    DB_NAME,
+    DB_USER,
+    DB_PASS,
+    SERVER_HOST,
+    DB_DIALECT
+} = process.env
+
+module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: SERVER_HOST,
+    dialect: DB_DIALECT
+})
