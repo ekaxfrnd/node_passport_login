d69ae6c5bfb8afac8281b9604fb4c42d7f99c65e
ekaxfrnd <ekaxfrnd@gmail.com>
2020-09-13 14:16:10 -0400

    create User model for users table in db

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+
+const conn = require('../config/conn')
+
+const User = conn.define('user', {
+    first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+User.sync()
+
+module.exports = User
